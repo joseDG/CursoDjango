3a8d0a84393bb552cbb19303ea2a96ff8e388cbe
JoseDG <josethp1@gmail.com>
2024-03-08 20:57:53 -0500

    clase de refactorizacion en templates urls y uso de las vistas genericas

--- core/urls.py
+++ core/urls.py
@@ -15,13 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 #importacion de la vista
 from . import views
 
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    
+    #url de la app home
+    path('', include('applications.home.urls')),
+    
 ]
