91ac42d253d26d6ec3540bc0004879d11f49885c
JoseDG <josethp1@gmail.com>
2024-03-13 19:12:38 -0500

    creacion de modelos

--- applications/home/models.py
+++ applications/home/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Prueba(models.Model):
   titulo = models.CharField(max_length=30)
   subtitulo = models.CharField(max_length=50)
+
+  #muestra el nombre en el panel del administrador
+  def __str__(self):
+    return self.titulo
